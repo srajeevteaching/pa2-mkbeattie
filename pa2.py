c36105376d66af05483b2efc50bc394f10d3cbb5
Katie Beattie <mkbeattie@loyola.edu>
2021-10-06 23:09:55 -0400

    did it again!

--- pa2.py
+++ pa2.py
@@ -9,35 +9,36 @@ monthStr = input("Enter a month: ")
 month = int(monthStr)
 yearStr = input("Enter a year: ")
 year = int(yearStr)
-if month == 1:
-    mDays = 31
-elif month == 2:
-    if year % 4 == 0:
-        year_isLeapYear = True
-        mDays = 29
-    else:
-        year_isLeapYear = False
-        mDays = 28
-elif month == 3:
-    mDays = 31
-elif month == 4:
-    mDays = 30
-elif month == 5:
-    mDays = 31
-elif month == 6:
-    mDays = 30
-elif month == 7:
-    mDays = 31
-elif month == 8:
-    mDays = 31
-elif month == 9:
-    mDays = 30
-elif month == 10:
-    mDays = 31
-elif month == 11:
-    mDays = 30
-elif month == 12:
-    mDays = 31
+if (1 <= month <= 12) and (year >= 1):
+    if month == 1:
+        mDays = 31
+    elif month == 2:
+        if year % 4 == 0:
+            year_isLeapYear = True
+            mDays = 29
+        else:
+            year_isLeapYear = False
+            mDays = 28
+    elif month == 3:
+        mDays = 31
+    elif month == 4:
+        mDays = 30
+    elif month == 5:
+        mDays = 31
+    elif month == 6:
+        mDays = 30
+    elif month == 7:
+        mDays = 31
+    elif month == 8:
+        mDays = 31
+    elif month == 9:
+        mDays = 30
+    elif month == 10:
+        mDays = 31
+    elif month == 11:
+        mDays = 30
+    elif month == 12:
+        mDays = 31
 else:
-    print("invalid month entry!")
+    print("invalid entry!")
 print("There are ", mDays, " days in that month.")
